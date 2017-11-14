fbeea14ee4a15fcd8429c620530a05ccfe22212d
Andrew Plaza <aplaza@liquidthink.net>
2017-11-13 19:37:19 -0500

    writing tests for deploying contracts

--- src/contract.rs
+++ src/contract.rs
@@ -6,11 +6,12 @@ use web3::transports::http::Http;
 use web3;
 use rustc_hex::FromHex;
 
-struct DebugContract {
-    acc_addr: Address,
-    contract_addr: Option<Address>,
+pub struct DebugContract {
+    pub acc_addr: Address,
+    pub contract_addr: Option<Address>,
     web3: web3::Web3<Http>,
     data: Vec<u8>,
+    pub contract: Option<Contract<Http>>,
 }
 
 /**
@@ -22,18 +23,16 @@ impl DebugContract {
      * Initializes new Contract to debug
      * acc_address: Your account address, hex
      */
-    fn new(acc_addr: String, contract_addr: Option<String>, provider: String, contract_path: String) 
+    #[allow(dead_code)]
+    pub fn new(acc_addr: Address, contract_addr: Option<String>, provider: String) 
     -> DebugContract 
     {
         let ( _eloop, http ) = web3::transports::Http::new(&provider).unwrap();
         let web3 = web3::Web3::new(http);
-
-        let acc_addr: Address = acc_addr.parse().unwrap();
-        let bytecode: Vec<u8> = include_str!("./Greeter.sol").from_hex().unwrap();
-
+        let bytecode: Vec<u8> = include_str!("./contract.code").from_hex().unwrap();
         let c_addr;
         if let Some(addr) = contract_addr {
-            c_addr = addr;
+            c_addr = Some(addr.parse().unwrap());
         } else { c_addr = None }
 
         DebugContract {
@@ -41,12 +40,27 @@ impl DebugContract {
             contract_addr: c_addr,
             web3,
             data: bytecode,
+            contract: None,
         }
     }
 
     /// Deploys a contract to the testRPC
-    pub fn deploy() {
-    
+    pub fn deploy(mut self) {
+       self.contract = Some(Contract::deploy(self.web3.eth(), include_bytes!("./contract.abi")).unwrap()
+           .confirmations(4)
+           .options(Options::with(|mut opt| opt.gas = Some(5_000_000.into())))
+           .execute(self.data, (
+                U256::from(1_000_000),
+                "Debug".to_owned(),
+                3u64,
+                "D".to_owned()
+                ), self.acc_addr)
+           .expect("Correct parameters are passed to the constructor.")
+           .wait()
+           .unwrap());
+       let result = self.contract.unwrap().query("greet", (self.acc_addr, ), None, Options::default(), None);
+       let greeting: U256 = result.wait().unwrap();
+       println!("Greeting: {:?}", greeting);
     }
 
     /// Sends a test transaction to the testRPC
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,33 @@ extern crate rustc_hex;
 mod contract;
 
 #[cfg(test)]
+// Test Convention
+// test_<Struct Name>_<Fn Name>
 mod tests {
+    use ::contract::DebugContract;
+    use web3::api::Eth;
+    use web3::transports::http::Http;
+    use web3::helpers::CallResult;
+    use web3::futures::Future;
+    use web3;
+
     #[test]
     fn it_works() {
+        println!("It works!");
+    }
+    #[test]
+    fn test_contract_deploy() {
+        let ( _eloop, http ) = web3::transports::Http::new("http://localhost:8545").unwrap();
+        let web3 = web3::Web3::new(http);
+        
+        println!("Coinbase Addr: {:?}", web3.eth().coinbase().wait().unwrap());
+        let debugContract = DebugContract::new(
+            web3.eth().coinbase().wait().unwrap(),
+            None, 
+            "http://localhost:8545".to_string()
+        );
+        println!("This means I must get to here");
+        debugContract.deploy();
     }
 }
-
-
 
