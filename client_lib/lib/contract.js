2f986701f1c1a1a47ed134f8019a35c26c0a5ff2
Andrew Plaza <andrew.plaza@protonmail.com>
2017-11-14 19:23:01 -0500

    few more ergonomics changes

--- client_lib/lib/contract.js
+++ client_lib/lib/contract.js
@@ -4,10 +4,18 @@ var fs = require("fs");
 var solc = require("solc");
 const eth_new_contract = require("eth-new-contract");
 
+
+/**
+ *@author Andrew Plaza
+ *
+ * Class for deploying and testing smart contracts to the testRPC
+ * uses web3 version 0.20.0 API 
+ * large changes to web3 API in v1.0.0betaX api
+ */
 class Contract {
   
-  /*
-   *
+  /**
+   *@author Andrew Plaza
    * @param{provider} http provider (http://localhost:8545
    * @param{contract_path} path of the contract
    * @param{options} options, TBA
@@ -22,7 +30,11 @@ class Contract {
     this.contract = null;
     this.options = options;
   }
-  
+  /**
+   * @author Andrew Plaza
+   * deploys a contract to the blockchain (TestRPC)
+   *
+   */
   deploy() {
     const input = fs.readFileSync(this.contract_path, 'utf8');
     const output = solc.compile(input.toString());
@@ -35,16 +47,24 @@ class Contract {
       from: this.web3.eth.coinbase
     });
   }
-
+  /**
+   * test if an arbitrary contract
+   * works correctly
+   * @param{cb} callback function
+   *  parameters available to callback:
+   *    @param{contract} the deployed contract
+   */
   test(cb) {
     let test_contract = this.web3.eth.contract(this.contract.abi);
     let test_contract_instance = test_contract.at(this.contract.address);
-    let result = test_contract_instance.greet('hello');
-    console.log(result);
-    cb();
+    cb(test_contract);
   }
 }
 
 let contract = new Contract('http://localhost:8545', './../../examples/example_solidity/Greeter.sol', ':greeter');
 contract.deploy();
-contract.test();
+
+contract.test((contract) => {
+  let result = contract.greet('hello');
+  console.log(result);
+});
