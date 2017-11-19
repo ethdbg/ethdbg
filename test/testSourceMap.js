f68c3782317de2ba21eb00f9ae9877b3ea4cb640
Andrew Plaza <aplaza@liquidthink.net>
2017-11-19 12:07:39 -0500

    convert to es6

--- test/testSourceMap.js
+++ test/testSourceMap.js
@@ -5,11 +5,11 @@ const solc = require('solc');
 
 
 function test() {
-  let path = './../examples/example_solidity/simple.sol';
+  let path = './../examples/example_solidity/Greeter.sol';
   let source = fs.readFileSync(path, 'utf8');
   let compiledSource = solc.compile(source, 1);
-  let bytecode = compiledSource.contracts[':SimpleStorage'].runtimeBytecode;
-  let sourcemap = compiledSource.contracts[':SimpleStorage'].srcmapRuntime;
+  let bytecode = compiledSource.contracts[':greeter'].runtimeBytecode;
+  let sourcemap = compiledSource.contracts[':greeter'].srcmapRuntime;
   const decoder = new Decoder();
   let decompressedSrc = decoder.decompressAll(sourcemap);
   decompressedSrc.forEach((match)=> {
