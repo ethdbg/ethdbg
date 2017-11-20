de8d1a995dd5c56d535b707dfa6f5b29285379a1
Andrew Plaza <aplaza@liquidthink.net>
2017-11-20 03:55:29 -0500

    change timeout to 3000ms in sourcemaptest

--- test/testSourceMap.js
+++ test/testSourceMap.js
@@ -3,17 +3,16 @@ let SourceMap = require('../lib/source_map.js');
 let Contract = require('../lib/contract.js');
 let Logger = require('../lib/logger.js');
 
-// TODO: Figure out why mocha times out
-describe('SourceMap', () => {
-  describe('#getInstOffset()', () => {
-    it('should return a correct thingy', (done) => {
+describe('SourceMap', function() {
+  describe('#getInstOffset()', function() {
+    it('should return the correct line number', function() {
+      this.timeout(3000);
       let logger = new Logger(5);
       let contract = new Contract(null, './test/Simple.sol',
                                   'SimpleStorage', {});
       assert.equal(7,
       new SourceMap(contract, logger)
         .getInstOffset(7).startEnd.end.line);
-      done();
     });
   });
 });
