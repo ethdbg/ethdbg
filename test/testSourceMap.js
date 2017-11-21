6b65568e6c9797bf4432f49fa8e16264db9e56fb
Andrew Plaza <aplaza@liquidthink.net>
2017-11-21 05:20:27 -0500

    tests for breakpoint class

--- test/testSourceMap.js
+++ test/testSourceMap.js
@@ -1,9 +1,7 @@
+const {expect} = require('chai');
 const assert = require('assert');
 const SourceMap = require('./../lib/source_map');
-const Contract = require('./../lib/contract');
-const Logger = require('./../lib/logger');
-const Breakpoint = require('./../lib/breakpointManager');
-
+const Contract = require('./../lib/contract'); const Logger = require('./../lib/logger'); const Breakpoint = require('./../lib/breakpointManager'); 
 describe('SourceMap', function() {
   describe('#getInstOffset()', function() {
     it('should return the correct line number.', function() {
@@ -19,16 +17,53 @@ describe('SourceMap', function() {
 });
 
 describe('Breakpoint', function() {
+  describe('#constructor()', function() {
+    it('should return an object with 4 properties', function() {
+      let breakpoint = new Breakpoint();
+      expect(breakpoint).to.have.property('lineNum');
+      expect(breakpoint).to.have.property('srcRange');
+      expect(breakpoint).to.have.property('instOffset');
+      expect(breakpoint).to.have.property('map');
+    });
+  });
+  describe('#constructor()', function() {
+    it('should create an object with the correct 4 properties', function() {
+      const breakpoint = new Breakpoint(
+        7,
+        {start: 3, end: 4},
+        30,
+        {start: 182, length: 186, file: 1, jump: '-'}
+      );
+      expect(breakpoint.lineNum).to.equal(7);
+      expect(breakpoint.srcRange).to.deep.equal({start: 3, end: 4});
+      expect(breakpoint.instOffset).to.equal(30);
+      expect(breakpoint.map).to.deep.equal(
+      {
+        start: 182,
+        length: 186,
+        file: 1,
+        jump: '-',
+      });
+    });
+  });
   describe('#get()', function() {
     it('should return an object with the given attributes.', function() {
-      let breakpoint = new Breakpoint(3, 5, 8);
-      let obj = {};
-      obj.lineNum = 3;
-      obj.byteRange = 5;
-      obj.insNum = 8;
-      assert.equal(obj.lineNum, breakpoint.get().lineNum);
-      assert.equal(obj.byteRange, breakpoint.get().byteRange);
-      assert.equal(obj.insNum, breakpoint.get().insNum);
+      const breakpoint = new Breakpoint(
+        7,
+        {start: 3, end: 4},
+        30,
+        {start: 182, length: 186, file: 1}
+      );
+
+      expect(breakpoint.lineNum).to.equal(breakpoint.getLineNum());
+      expect(breakpoint.srcRange).to.deep.equal(breakpoint.getSrcRange());
+      expect(breakpoint.instOffset).to.equal(breakpoint.getInstOffset());
+      expect(breakpoint.map).to.deep.equal(breakpoint.getMap());
+
+      assert.equal(breakpoint.lineNum, breakpoint.get().lineNum);
+      assert.equal(breakpoint.srcRange, breakpoint.get().srcRange);
+      assert.equal(breakpoint.instOffset, breakpoint.get().instOffset);
+      assert.equal(breakpoint.map, breakpoint.get().map);
     });
   });
 });
