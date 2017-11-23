e95e9a5a3414622c599856a3241541da13175a8e
Sean Batzel <romulus108@protonmail.com>
2017-11-23 17:50:21 -0500

    I was so hopeful, too... Not sure why it's telling me that testRPC is undefined.

--- test/TestRPC.js
+++ test/TestRPC.js
@@ -1,13 +1,21 @@
 const TestRPC = require('./../lib/test_rpc');
 const Logger = require('./../lib/logger');
 const Contract = require('./../lib/contract');
-const {fork} = require('child_process');
-const {expect} = require('chai');
-/*
+const {
+  fork
+} = require('child_process');
+const {
+  expect
+} = require('chai');
+
 let origTestRPC = null;
 describe('TestRpc', function() {
   before(function() {
-    origTestRPC = fork('./testrpc/cli.js', [], {silent: true});
+    const logger = new Logger(5);
+    origTestRPC = fork('./testrpc/cli.js', [], {
+      silent: true
+    });
+    const testRPC = new TestRPC(logger);
   });
   after(function() {
     origTestRPC.kill('SIGINT');
@@ -19,46 +27,39 @@ describe('TestRpc', function() {
         'http://localhost:8546',
         './test/Simple.sol',
         'SimpleStorage',
-        logger,
-        {},
+        logger, {},
       );
-      const testRpc = new TestRPC(logger);
+      //const testRpc = new TestRPC(logger);
       expect(testRpc).to.have.property('forkedBlockchain');
       expect(testRpc).to.have.property('forkAddress');
       expect(testRpc).to.have.property('logger');
       expect(testRpc).to.have.property('readyEvent');
     });
-  });
-  describe('#initEvents()', function() {
     it('should initialize listeners to forked process w/o error', function() {
       const logger = new Logger(5);
       const contract = new Contract(
         'http://localhost:8546',
         './test/Simple.sol',
         'SimpleStorage',
-        logger,
-        {},
+        logger, {},
       );
-      const testRpc = new TestRPC(contract, logger);
+      //const testRpc = new TestRPC(contract, logger);
       testRpc.initEvents();
     });
-  });
-  describe('DeployContract', function() {
     it('should deploy the contract and halt at the first instruction',
-    function(done) {
-      const logger = new Logger(5);
-      const contract = new Contract(
-        'http://localhost:8546',
-        './test/Simple.sol',
-        'SimpleStorage',
-        logger,
-        {},
-      );
-      const testRpc = new TestRPC(contract, logger);
-      testRpc.initEvents();
-      testRpc.runContract(done);
-    });
+      function(done) {
+        const logger = new Logger(5);
+        const contract = new Contract(
+          'http://localhost:8546',
+          './test/Simple.sol',
+          'SimpleStorage',
+          logger, {},
+        );
+        //const testRpc = new TestRPC(contract, logger);
+        testRpc.initEvents();
+        testRpc.runContract(done, () => {
+          logger.debug("Contract tested.");
+        });
+      });
   });
 });
-
-*/
