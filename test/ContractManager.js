3a0801be82ea3080ebf22c6d7405ed4dbcb75cc7
Andrew Plaza <aplaza@liquidthink.net>
2017-11-28 23:59:25 -0500

    remove SourceMap requirement from TestRPC and GanacheWrapper

---
+++ test/ContractManager.js
@@ -0,0 +1,18 @@
+const {expect} = require('chai');
+const Logger = require('./../lib/logger');
+const 
+describe('ContractManager', function() {
+  describe('#constructor()', function() {
+    it('should instantiate a HashMap to hold contracts')'
+    it('should instantiate a populated HashMap if contracts are passed in opts');
+  });
+  describe('#add()', function() {
+    it('should add a contract to the HashMap');
+  });
+  describe('#getContract(contract_name)', function() {
+    it('should return')
+  });
+  describe('#getSource(source_file)', function() {
+    it('should get a contract by source file');
+  });
+});
