e27b619cb60d88f561da349c25610f86859c6582
Andrew Plaza <aplaza@liquidthink.net>
2017-11-29 02:17:30 -0500

    finish first rendition of ContractManager

--- test/ContractManager.js
+++ test/ContractManager.js
@@ -6,13 +6,28 @@ describe('ContractManager', function() {
     it('should instantiate a HashMap to hold contracts')'
     it('should instantiate a populated HashMap if contracts are passed in opts');
   });
-  describe('#add()', function() {
+  describe('#add(srcFPath: string)', function() {
     it('should add a contract to the HashMap');
   });
-  describe('#getContract(contract_name)', function() {
-    it('should return')
+  describe('#addByContract(contract: Contract)', function() {
+    it('should add a contract to the HashMap');
   });
-  describe('#getSource(source_file)', function() {
-    it('should get a contract by source file');
+  describe('#get(contractName: string)', function() {
+    it('should return Contract object')
+  });
+  describe('#getBySource(srcFPath: string)', function() {
+    it('should get contracts that belong to a source file');
+  });
+  describe('#normalize(contractName: string)', function() {
+    it('should normalize a contract name if it is prepended with :');
+  });
+  describe('#getContracts()', function() {
+    it('should return a HashMap of contracts');
+  });
+  describe('#remove(contractName: string)', function() {
+    it('should remove a contract from Contract hashmap');
+  });
+  describe('#removeBySource(srcFPath: string)', function() {
+    it('should remove all contracts associated with a source file');
   });
 });
