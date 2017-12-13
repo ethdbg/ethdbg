2dbcb4b298492db1e4873274a3149e95d9b70468
Sean Batzel <romulus108@protonmail.com>
2017-12-13 16:29:04 -0500

    Added variable/stacktrace interfaces.

--- lib/context.d.ts
+++ lib/context.d.ts
@@ -1,7 +1,18 @@
+export interface stackframe {
+	text: string;
+	file: string;
+	line: number;
+}
+
+export interface variable {
+	name: string;
+	value: number;
+}
+
 export default class Context {
 
-	public variables: Array<Object>;
-	public stack: Array<Object>;
+	public variables: Array<variable>;
+	public stack: Array<stackframe>;
 
 	constructor(line: number, variables: Object, exception: string, errInfo: string, stack: Object);
 }
