45f9eea2bdd9f29c7a68ecf694c91e6bc42fdc7a
Andrew Plaza <aplaza@liquidthink.net>
2017-12-16 00:23:51 -0500

    add more exports to index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Debugger = require('./lib/debugger.js');
+const DebugProvider = require('./lib/debug_provider.js');
+const types = require('./lib/types.js');
 
 module.exports = Debugger;
 exports.default = Debugger;
