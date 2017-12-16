f157f974fdcdc5c74a7089c879b85c602bf5e088
Andrew Plaza <aplaza@liquidthink.net>
2017-12-16 00:26:16 -0500

    change exports in index file

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const Debugger = require('./lib/debugger.js');
 const DebugProvider = require('./lib/debug_provider.js');
 const types = require('./lib/types.js');
 
-module.exports = Debugger;
+module.exports = {
+  Debugger,
+  DebugProvider,
+  types,
+};
 exports.default = Debugger;
+exports.DebugProvider = DebugProvider;
+exports.types = types;
