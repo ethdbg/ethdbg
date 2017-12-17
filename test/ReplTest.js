c311438c1bb5618c72e89537c43023b46c3ed2db
Sean Batzel <romulus108@protonmail.com>
2017-12-17 15:05:45 -0500

    Once again, fixed an issue with the REPL, hopefully this test passes.

--- test/ReplTest.js
+++ test/ReplTest.js
@@ -1,13 +1,12 @@
 const Logger = require('./../lib/logger');
 const REPL = require('./../lib/repl');
 const GanacheWrapper = require('./../lib/ganache_wrapper');
-const {events} = require('./../lib/types');
 
 
 describe('REPL', function () {
     describe('#execute()', function () {
-        it('should return the correct line number.', function () {
-            let logger = new Logger(events.loggerLevels.DEBUG);
+        it('should return a result of 1.', function () {
+            let logger = new Logger(5);
             let testRPC = new GanacheWrapper(logger);
             let repl = new REPL("(2**800 + 1) - 2**800");
             expect(repl).to.eql(1);
