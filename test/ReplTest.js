9ba34c9d664e68ef20db094069aa450981c8f2dc
Sean Batzel <romulus108@protonmail.com>
2017-12-17 14:45:23 -0500

    Try this again. We're gonna need to squash this.

--- test/ReplTest.js
+++ test/ReplTest.js
@@ -1,11 +1,13 @@
 const Logger = require('./../lib/logger');
 const REPL = require('./../lib/repl');
 const GanacheWrapper = require('./../lib/ganache_wrapper');
+const {events} = require('./types');
 
 
-describe('SourceMap', function () {
-    describe('#getInstOffset()', function () {
+describe('REPL', function () {
+    describe('#execute()', function () {
         it('should return the correct line number.', function () {
+            let logger = new Logger(events.loggerLevels.DEBUG);
             let testRPC = new GanacheWrapper(logger);
             let repl = new REPL("(2**800 + 1) - 2**800");
             expect(repl).to.eql(1);
