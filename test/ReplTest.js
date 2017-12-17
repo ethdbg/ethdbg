1c9e72d321da97d6fb0321569229667dc429d76c
Sean Batzel <romulus108@protonmail.com>
2017-12-17 15:18:33 -0500

    -_-

--- test/ReplTest.js
+++ test/ReplTest.js
@@ -8,7 +8,7 @@ describe('REPL', function () {
         it('should return a result of 1.', function () {
             let logger = new Logger(5);
             let testRPC = new GanacheWrapper(logger);
-            let repl = new REPL("(2**800 + 1) - 2**800");
+            let repl = new REPL().execute("(2**800 + 1) - 2**800");
             expect(repl).to.eql(1);
         });
     });
