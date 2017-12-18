19c04a15a9408e00f4de6bae3d1ad291267e6312
Sean Batzel <romulus108@protonmail.com>
2017-12-17 19:02:51 -0500

    Testing contract functionality.

---
+++ test/contract.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const Logger = require('./../lib/logger');
+const Contract = require('./../lib/contract');
+
+describe('Contract', function () {
+    describe('#constructor(logger, name, options)', function () {
+        it('should instantiate class successfully given source.', function () {
+            const logger = new Logger(1);
+            const options = {
+                'source': "(2**800 + 1) - 2**800"
+            }
+            const contract = new Contract(logger, 'test');
+        });
+    });
+});
