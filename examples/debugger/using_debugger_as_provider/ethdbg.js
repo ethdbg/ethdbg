b61e6c3a2bcb94d53e53495640928fa675049927
insi <aplaza@liquidthink.net>
2017-12-19 15:35:16 -0500

    updated ethdbg spawn example

--- examples/debugger/using_debugger_as_provider/ethdbg.js
+++ examples/debugger/using_debugger_as_provider/ethdbg.js
@@ -1,11 +1,8 @@
 #! /usr/bin/env node
-// const { DebugProvider } = require('./../ethdbg/index');
-const DebugProvider = require('./../../../lib/debug_provider');
+// TODO remove alot of the boilerplate by moving it into ethdbg main
+const {DebugProvider} = require('/home/insi/Projects/ETHDBG/ethdbg/index.js');
 const yargs = require('yargs');
-
-/**
- * this is what is going to be spawned; hence it does not need to be TS
- */
+const _ = require('lodash');
 
 /**
   Available Options (taken directly from ganache-cli:
@@ -54,6 +51,7 @@ const parser = yargs()
     type: 'string',
     alias: 'u'
   });
+ 
 let argv = parser.parse(process.argv);
 
 if (argv.d || argv.deterministic) {
@@ -137,10 +135,17 @@ function parseAccounts(accounts) {
 
 (() => {
   try {
-    const ethdbg = new DebugProvider(options);
+    let opts = {};
+    _.forIn(options, (val, key) => {  // conv string bools to intrinsic types
+      if (options[key] === 'false' || options[key] === 'true') {
+        opts[key] = (val == 'true'); 
+      } else {
+        opts[key] = val;
+      }
+    });
+    const ethdbg = new DebugProvider(opts);
     ethdbg.run();
   } catch (err) {
     throw new Error(`Error in ethdbg ${err}`);
   }
- })();
-
+})();
