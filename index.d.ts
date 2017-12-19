2ffd6d7656c51406ab16e5a521e7d82f4619af30
insi <aplaza@liquidthink.net>
2017-12-18 22:02:32 -0500

    add events to typings

--- index.d.ts
+++ index.d.ts
@@ -36,12 +36,35 @@ declare namespace ethdbg {
     
     serialize(ev: string, data: Object): string;
   }
+
+  export enum events {
+    isReady=            'isReady',
+    ready=              'ready',
+    hitBreakpoint=      'hitBreakpoint',
+    addBreakpoints=     'addBreakpoints',
+    removeBreakpoints=  'removeBreakpoints',
+    clearAllBreakpoints='clearAllBreakpoints',
+    toggleBreakpoint=   'toggleBreakpoint',
+    addFiles=           'addFiles',
+    start=              'start',
+    stop=               'stop',
+    continue=           'continueExecution',
+    stepInto=           'stepInto',
+    stepOut=            'stepOut',
+    stepOver=           'stepOver',
+    getVarList=         'getVarList',
+    restart=            'restart',
+    kill=               'EXECUTE_ORDER_66',
+    message=            'message',
+  }
 }
+
 declare var Debugger: ethdbg.Debugger;
 declare var DebugProvider: ethdbg.DebugProvider;
-
+declare var events: ethdbg.events;
 
 export {
   Debugger,
   DebugProvider,
+  events,
 };
