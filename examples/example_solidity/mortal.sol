c9658c2443087e1f38904855d46a9de8e9b0ee48
Sean Batzel <romulus108@protonmail.com>
2017-12-21 00:47:35 -0500

    Added a useless compiler test.

---
+++ examples/example_solidity/mortal.sol
@@ -0,0 +1,13 @@
+contract mortal {
+
+    address owner;
+
+    function mortal() {
+        owner = msg.sender;
+    }
+ 
+    function kill() {
+        if (msg.sender == owner)
+            selfdestruct(owner);
+    }
+}
