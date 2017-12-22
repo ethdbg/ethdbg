595ff6b01ff557e7a54321e5eedddb556d3288a4
Sean Batzel <romulus108@protonmail.com>
2017-12-22 16:38:40 -0500

    Fixed tests and a stupid mistake I made in the last couple commits.

--- test/ReplTest.js
+++ test/ReplTest.js
@@ -15,14 +15,13 @@ describe('REPL', function () {
                 .catch(err => {
                     console.log(err)
                 });
-            expect(res).to.equal(2);
         });
         it('should return the correct value for 1+1===2', function () {
             let repl = REPL();
             let res = repl('uint a = 1; uint b = 1; a + b;')
                 .then(result => {
                     if (result !== null) {
-                        assert(result === 2);
+                        expect(result).to.equal(2);
                     }
                 })
         });
