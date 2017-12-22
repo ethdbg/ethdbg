8e3410aa6f2e277a8fa592bb42606093f8b05dae
Sean Batzel <romulus108@protonmail.com>
2017-12-21 19:17:45 -0500

    REPL doesn't use objects.

--- test/ReplTest.js
+++ test/ReplTest.js
@@ -7,7 +7,7 @@ describe('REPL', function () {
     describe('#execute(machine: ethereumjs-vm, code: string)', function () {
         it('should pass silently', function () {
             let machine = new VM();
-            let repl = new REPL();
+            let repl = REPL();
             repl('1+1')
                 .then(result => {
                     // if null is returned, it means the last command was not an expression with a return value
@@ -21,7 +21,7 @@ describe('REPL', function () {
         });
         it('should return a valid response from solidity functions', function () {
             let machine = new VM();
-            let repl = new REPL();
+            let repl = REPL();
             let res = repl('1+1')
                 .then(result => {
                     // if null is returned, it means the last command was not an expression with a return value
