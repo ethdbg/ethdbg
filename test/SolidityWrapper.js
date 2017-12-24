3737d05ea50fd3b25913aa19e4b5a164a60ba012
Sean Batzel <romulus108@protonmail.com>
2017-12-23 22:41:07 -0500

    Added test descriptions.

--- test/SolidityWrapper.js
+++ test/SolidityWrapper.js
@@ -2,19 +2,19 @@ const { expect } = require('chai');
 const SolidityWrapper = require('./../lib/solidity_wrapper');
 describe('SolidityWrapper', function () {
     describe('#constructor(sourcePath: string, name: string)', function () {
-        it('', function () {
+        it('should instantiate a new SolidityWrapper without issue', function () {
         });
     });
     describe('#getSource()', function () {
-        it('', function () {
+        it('return the source of this contract only', function () {
         });
     });
     describe('#getLinkedSource()', function () {
-        it('', function () {
+        it('return the body of all contract source', function () {
         });
     });
     describe('#getImports(source: string, cwd: string, sf: string)', function () {
-        it('', function () {
+        it('return the array of imported contracts', function () {
         });
     });
 });
