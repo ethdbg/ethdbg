5503dc7b04f5bbdf4466c9dc0f4a05f1b3f47521
Sean Batzel <romulus108@protonmail.com>
2017-12-23 22:37:00 -0500

    Began tests for the SolidityWrapper.

---
+++ test/SolidityWrapper.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const SolidityWrapper = require('./../lib/solidity_wrapper');
+describe('SolidityWrapper', function () {
+    describe('#constructor(sourcePath: string, name: string)', function () {
+        it('', function () {
+        });
+    });
+    describe('#getSource()', function () {
+        it('', function () {
+        });
+    });
+    describe('#getLinkedSource()', function () {
+        it('', function () {
+        });
+    });
+    describe('#getImports(source: string, cwd: string, sf: string)', function () {
+        it('', function () {
+        });
+    });
+});
